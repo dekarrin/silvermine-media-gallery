051cc0379fa7cc997be78a19890d18d954e048bd
TF Nelson <tails.fanatic@gmail.com>
2014-02-15 12:50:45 +0000

    Made daemon reset affect all non-successes

--- converter/reset_conversions.php
+++ converter/reset_conversions.php
@@ -7,5 +7,5 @@ define('IN_COPPERMINE', true);
 include 'include/init.inc.php';
 include 'include/video_convert.php';
 
-cpg_db_query("UPDATE {$CONFIG['TABLE_CONVERSIONS']} SET status='".CONVERT_STATUS_READY."', error_message=NULL WHERE status='".CONVERT_STATUS_FAILURE."'");
+cpg_db_query("UPDATE {$CONFIG['TABLE_CONVERSIONS']} SET status='".CONVERT_STATUS_READY."', error_message=NULL WHERE status!='".CONVERT_STATUS_SUCCESS."'");
 ?>
