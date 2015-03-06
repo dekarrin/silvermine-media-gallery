a86a521f698be64fb173424dfb63eb7b4a4a00c5
TF Nelson <tails.fanatic@gmail.com>
2015-03-06 09:12:28 -0600

    Added error reporting after init.inc.php was included.\(including it suppresses error reporting)

--- converter/get_daemon_report.php
+++ converter/get_daemon_report.php
@@ -5,9 +5,12 @@ if (defined('IN_COPPERMINE')) {
 define('IN_COPPERMINE', true);
 
 require 'include/init.inc.php';
+error_reporting(E_ALL);
 require 'include/video_convert.php';
 
 $result = cpg_db_query("SELECT cid,error_message FROM {$CONFIG['TABLE_CONVERSIONS']}");
+$rows = mysql_num_rows($result);
+echo "$rows conversions:\n\n";
 while ($row = mysql_fetch_assoc($result)) {
 	echo $row['cid'] . ":\n";
 	switch ($row['status']) {
--- converter/reset_conversions.php
+++ converter/reset_conversions.php
@@ -5,7 +5,9 @@ if (defined('IN_COPPERMINE')) {
 define('IN_COPPERMINE', true);
 
 require 'include/init.inc.php';
-require 'include/video_convert.php';
+error_reporting(E_ALL);
+
+require 'include/video_convert.inc.php';
 
 cpg_db_query("UPDATE {$CONFIG['TABLE_CONVERSIONS']} SET status='".CONVERT_STATUS_READY."', error_message=NULL WHERE status!='".CONVERT_STATUS_SUCCESS."'");
 ?>
